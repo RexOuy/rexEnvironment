e805999212df387f71a3335fe6ae99075e3b218d
ouyangHuan <849652207@qq.com>
2021-11-09 13:12:33 +0800

    2.1作答

---
+++ thinkInJavaDemo/src/main/java/Chapter2/Answer1.java
@@ -0,0 +1,39 @@
+package Chapter2;
+
+/**
+ * 2.1
+ * @Author RexOuy
+ * @Date 2021/11/9
+ */
+public class Answer1 {
+    private int intValue;
+    private double doubleValue;
+
+    public int getIntValue() {
+        return intValue;
+    }
+
+    public double getDoubleValue() {
+        return doubleValue;
+    }
+
+    /**
+     * 答案外,看看局部变量有啥反应
+     * @return int
+     */
+    public int isInit() {
+        int innerValue;
+        Integer innerValue2;
+//        return innerValue;   局部变量不会给定默认值
+//        return innerValue2;  null都不给= =
+        return 0;
+    }
+
+
+    public static void main(String[] args) {
+        Answer1 answer1 = new Answer1();
+        System.out.println("int: "+ answer1.getIntValue());
+        System.out.println("double: " + answer1.getDoubleValue());
+    }
+
+}
