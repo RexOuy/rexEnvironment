9b4417f8bd181d5a602416ff45d260791115b82b
oyh <849652207@qq.com>
2021-11-16 12:11:26 +0800

    5-17

---
+++ thinkInJavaDemo/src/main/java/net/rexouy/thinkinjavademo/Chapter5/Answer17.java
@@ -0,0 +1,18 @@
+package net.rexouy.thinkinjavademo.Chapter5;
+
+/**
+ * 5-17
+ * String数组相关
+ * @Author RexOuy
+ * @Date 2021/11/9
+ */
+public class Answer17 {
+    Answer17(String value){
+        System.out.println(value);
+    }
+
+    public static void main(String[] args) {
+        Answer17[] ges;
+    }
+    //数组设置的引用类型，并没有创建该类型的对象
+}
